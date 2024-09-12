41d25e526c271f81feccc5143917d6a221e033e1
codingruo <r.schober@outlook.com>
2024-09-12 20:59:54 +0200

    First Compilation

---
+++ Targets/Debug/EmpyEditor/CMakeFiles/Empy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/Empy"
+  "../lib/Empy.pdb"
+  "CMakeFiles/Empy.dir/src/Editor.cpp.o"
+  "CMakeFiles/Empy.dir/src/Editor.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Empy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
